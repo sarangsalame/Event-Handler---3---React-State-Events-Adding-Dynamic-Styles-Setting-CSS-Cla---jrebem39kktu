0814fd15fdc73b5ecd762d39435f4fbeab8995d7
sarang salame <115799065+sarangsalame@users.noreply.github.com>
2022-12-18 21:17:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,16 @@ import React from 'react'
 import '../styles/App.css';
 import React, { useState } from "react";
 const App = () => {
+  
+  let [data, SetData]=useState({text:"",num:""})
 
-  const handleInput = (event) =>{
+  const handleInput = (e) =>{
    // use console.log
     {
-      console.log(event.target.value);
+       Setdata({
+      ...data,
+      [e.target.name]:e.target.value
+    })
   };
   }
 
@@ -14,13 +19,13 @@ const App = () => {
   return (
     <div id="main">
       <label htmlFor='text-input'>Text Input:- </label>
-      <input id="text-input" type={'text'} onChange={handleInput}/>
+      <input name="text" id="text-input" type={'text'} onChange={handleInput}/>
 
       <br/>
       <br/>
 
       <label htmlFor='num-input'>Number input</label>
-      <input id="num-input"  type={'number'} />
+      <input name="num" id="num-input" onClick={handleInput} type={'number'} />
       <br/>
     </div>
   )
